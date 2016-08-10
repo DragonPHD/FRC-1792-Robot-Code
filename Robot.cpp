58b9da4f4717e0c4f7ad474c2cf7684f18890b1b
Donovan <cplusdragon@gmail.com>
2016-08-09 19:47:12 -0500

    Pixy Testing

--- Robot.cpp
+++ Robot.cpp
@@ -1,13 +1,18 @@
 #include "SPI.h"
-//#include "Pixy.h"
+#include "Pixy.h"
 #include "WPILib.h"
 
 class Robot: public IterativeRobot
 {
-	//Pixy pixy;
+	Pixy pixy;
 	RobotDrive myRobot; // robot drive system
 	Joystick stick,xbox; // only joystick
 	LiveWindow *lw;
+	VictorSP testmotor;
+	Servo testservo;
+
+	float speed,angle;
+	bool work;
 
 public:
 	void RobotInit() override {
@@ -17,7 +22,13 @@ public:
 		myRobot(4,5),	//Originally (1,0)
 		stick(5),		//Needs to be in USB 5
 		xbox(0),
-		lw(LiveWindow::GetInstance())
+		lw(LiveWindow::GetInstance()),
+		testmotor(0),
+		testservo(1),
+
+		speed(0),
+		angle(0),
+		work(0)
 
 	{
 		myRobot.SetExpiration(0.1); //Code from Example
@@ -41,7 +52,36 @@ private:
 
 	void TeleopPeriodic()	//Teleoperated Begins
 	{
+		//angle=stick.GetRawAxis(0);
+		//angle=angle+1;
+		//angle=angle*180;
+		if(stick.GetRawButton(12)&&!work)
+		{
+			angle=angle+1;
+			work=true;
+		}
+		else if(stick.GetRawButton(11)&&!work)
+		{
+			angle=angle-1;
+			work=true;
+		}
+		else if(!stick.GetRawButton(12)&&!stick.GetRawButton(11)&&work)
+		{
+			work=false;
+		}
 
+		pixy.getBlocks(1);
+
+
+
+
+		speed=stick.GetRawAxis(1);
+		testmotor.SetSpeed(speed);
+		testservo.Set(angle);
+		SmartDashboard::PutNumber("X",pixy.blocks[0].x);
+		SmartDashboard::PutNumber("Y",pixy.blocks[0].y);
+		SmartDashboard::PutNumber("Angle",angle);
+		SmartDashboard::PutNumber("Speed",speed);
 	}
 
 	//Dunno what to do with this
